a23208ba904a4022a84beb9f835bc3d31420e216
LouisPino <ljpino95@gmail.com>
2023-09-03 22:04:05 -0400

    vid link

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,9 +8,8 @@ function Landing(props) {
     <h2 id="welcome">Louis Pino</h2>
     <Link to="/about"><button className="welcome-btn code-enter"><p className="welcome-btn-text">CODE</p></button></Link>
     <a href="https://www.louispino.com" target="_blank"><button className="welcome-btn music-enter"><p className="welcome-btn-text">MUSIC</p></button></a>
-<video className="chorale" src="/images/invert.mov" muted autoPlay loop></video>
-{/* <video className="chorale"  type="video/youtube" src="https://www.youtube.com/watch?v=QUNrBEhvXWQ" muted autoPlay loop></video> */}
-
+    
+    <iframe className="chorale" src="https://www.youtube.com/embed/9fX7OuVV4Tc?si=3QepVtMwVfLCb5xK&amp;controls=0&amp;start=5&amp;autoplay=1&amp;mute=1&amp;playsinline=1&amp;playlist=9fX7OuVV4Tc&amp;loop=1&amp;showinfo=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen ></iframe>
     </div>
     
     )
