9d0185c642087cdbb6d0b210939056628fd22ff8
LouisPino <ljpino95@gmail.com>
2023-09-18 15:57:07 -0400

    fix burger menu

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,29 +2,22 @@ import Skills from "../components/Skills"
 import { useState, useEffect } from "react";
 
 function AboutMe() {
-  // create state to hold about data
   const [about, setAbout] = useState(null);
-
-  // create function to make api call
   const getAboutData = async () => {
-
-		// make api call and get response
     const response = await fetch("./about.json");
-
-		// turn response into javascript object
     const data = await response.json();
-
-		// set the about state to the data
     setAbout(data);
   };
 
-  // make an initial call for the data inside a useEffect, so it only happens once on component load
   useEffect(() => { getAboutData() } , []);
+  useEffect(() => { 
+    const gifEl = document.querySelector(".gif-ctr")
+    const bioEl = document.querySelector("#about-bio")
+    const aboutEl = document.querySelector(".about-main")
+  } , [about]);
 
-  // define a function that will return the JSX needed once we get the data
   const loaded = () => (
-    <div className="about-main">
-        
+    <div className="about-main">  
   <div className="gif-ctr">
   <img src="/images/JazGif.gif"/>
 </div>
@@ -35,7 +28,9 @@ function AboutMe() {
     </div>
   );
 
-  // if data arrives return the result of loaded, if not, an h1 that says loading
+
+
+
   return about ? loaded() : <h1>Loading...</h1>;
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,28 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createRef } from "react";
 
 export default function Skills(){
     const [about, setAbout] = useState(null);
-
-    // create function to make api call
     const getAboutData = async () => {
-  
-          // make api call and get response
       const response = await fetch("./about.json");
-  
-          // turn response into javascript object
       const data = await response.json();
-  
-          // set the about state to the data
       setAbout(data);
     };
-  
-    // make an initial call for the data inside a useEffect, so it only happens once on component load
-    useEffect(() => { getAboutData() } , []);
-  
+
+    useEffect(() => { getAboutData() } , []);  
+
+
+
 if(about){
 const skillsList = about.skills.map((skill)=>(
     <div className="skill-card">
-        <img className="skill-logo" src={skill.logo}/>
+        <img className="skill-logo " src={skill.logo}/>
     <h4>{skill.skill}</h4>
     </div>
     )   
@@ -33,7 +26,5 @@ const skillsList = about.skills.map((skill)=>(
     </div>
     )
 }
-
-
 
 }
