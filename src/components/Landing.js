7ed52b96e8023a245e73848758aa8232c74404fb
LouisPino <ljpino95@gmail.com>
2023-09-19 10:00:58 -0400

    test playsInline

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,7 +9,7 @@ function Landing(props) {
     <Link to="/about"><button className="welcome-btn code-enter"><p className="welcome-btn-text">CODE</p></button></Link>
     <a href="https://www.louispino.com" target="_blank"><button className="welcome-btn music-enter"><p className="welcome-btn-text">MUSIC</p></button></a>
     
-    <iframe className="chorale" src="https://www.youtube.com/embed/9fX7OuVV4Tc?si=3QepVtMwVfLCb5xK&amp;controls=0&amp;start=0&amp;autoplay=1&amp;mute=1&amp;playsinline=1&amp;playlist=9fX7OuVV4Tc&amp;loop=1&amp;showinfo=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
+    <iframe className="chorale" src="https://www.youtube.com/embed/9fX7OuVV4Tc?si=3QepVtMwVfLCb5xK&amp;controls=0&amp;start=0&amp;autoplay=1&amp;mute=1&amp;playsinline=1&amp;playlist=9fX7OuVV4Tc&amp;loop=1&amp;showinfo=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen playsInline></iframe>
     </div>
     
     )
