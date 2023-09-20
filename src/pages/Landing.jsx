303b85a7ebe90ba132c3fc501400f0fcc7422034
LouisPino <ljpino95@gmail.com>
2023-09-19 21:07:00 -0400

    welcome page gif instead of video

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,7 +4,7 @@ import Welcome from "../components/Welcome"
 export default function Home(){
 return(
 <>
-<Welcome message="WELCOME TO MY PORTFOLIO, COME ON IN!" time={2000}/>
+<Welcome message="WELCOME TO MY PORTFOLIO, COME ON IN!" time={1000}/>
 <Landing />
 </>
 
