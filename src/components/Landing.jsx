5735bedd1ac7363cbd8eb45f3b782e341cee1729
LouisPino <ljpino95@gmail.com>
2023-10-25 19:21:01 -0400

    fix welcome buttons on mobile

---
+++ src/components/Landing.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+
+function Landing(props) {
+    return (
+        <div className="home-main">
+            <h2 id="welcome">LOUIS PINO</h2>
+            <div className="welcome-btns">
+                <Link to="/about"><button className="welcome-btn code-enter"><p className="welcome-btn-text">CODE</p></button></Link>
+                <a href="https://www.louispino.com" target="_blank"><button className="welcome-btn music-enter"><p className="welcome-btn-text">MUSIC</p></button></a>
+            </div>
+            <img className="chorale" src="/images/welcomegif.gif" />
+        </div>
+
+    )
+}
+
+export default Landing;
