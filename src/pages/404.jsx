a816d5e8d797c2d95098a616eebdf9990ee95a9c
LouisPino <ljpino95@gmail.com>
2023-10-26 18:26:03 -0400

    fix 404 opacity

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,11 @@
 import "../App.css"
 
-export default function _404(){
-return (
+export default function _404() {
+    return (
 
-<div className="the404">
-<h1>There's nothing here! Enjoy this picture of Tilly.</h1>
-<img src="/images/Tilly.jpg" alt="" />
-</div>
-)
+        <div className="the404">
+            <h1>There's nothing here! Enjoy this picture of Tilly.</h1>
+            <img src="/images/Tilly2.jpg" alt="" />
+        </div>
+    )
 }
