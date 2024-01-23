51c86118c0ed5ddfbaf61c081b559a8482d5c9fe
LouisPino <ljpino95@gmail.com>
2024-01-23 10:34:34 -0500

    underline for selected page instead of glow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,20 +58,14 @@ function Header({ music, setMusic }) {
 
         <nav>
           <Link to="/about">
-            <button className="nav-btn about-btn" style={location === "ABOUT" ? {
-              color: "linear-gradient(90deg, red, orange, yellow, green, blue, purple)",
-              animation: "animate 15s linear infinite"
-            } :
-              {}
-            }><p className={`link nav-btn-text`}>ABOUT</p></button>
+            <button className="nav-btn about-btn">
+              <p className={location === "ABOUT" ? `link nav-btn-text selected-btn` : `link nav-btn-text `}>ABOUT</p>
+            </button>
           </Link>
           <Link to="/projects">
-            <button className="nav-btn projects-btn" style={location === "PROJECTS" ? {
-              color: "linear-gradient(90deg, red, orange, yellow, green, blue, purple)",
-              animation: "animate 15s linear infinite"
-            } :
-              {}
-            }><p className={`link nav-btn-text`}>PROJECTS</p></button>
+            <button className="nav-btn about-btn">
+              <p className={location === "PROJECTS" ? `link nav-btn-text selected-btn` : `link nav-btn-text `}>PROJECTS</p>
+            </button>
           </Link>
           <div className="music-btn-ctr">
             <button className="nav-btn music-btn" onClick={musicToggle}><p className="link nav-btn-text flippable">{music ? "CODE" : "MUSIC"}</p></button>
