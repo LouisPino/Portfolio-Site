0ae3e15b534edeccaf52e1bc4e4a3253bd6ac968
LouisPino <ljpino95@gmail.com>
2024-03-19 17:23:55 -0400

    test deploy

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -29,7 +29,7 @@ function Projects({ scrollPosition, setScrollPosition, music, setMusic }) {
 
   if (projects?.length) {
     let projectCards = projects.filter((project) => {
-      if (atAbout && !project.recent || project.feature) {
+      if (atAbout && !project.recent) {
         return false
       } else {
         if (music) {
@@ -39,7 +39,9 @@ function Projects({ scrollPosition, setScrollPosition, music, setMusic }) {
             return true
           }
         } else {
-          if (project.tech) {
+          if (project.tech && !atAbout && project.category.includes(musicChoice)) {
+            return true
+          } else if (project.tech && atAbout) {
             return true
           } else {
             return false
@@ -50,44 +52,60 @@ function Projects({ scrollPosition, setScrollPosition, music, setMusic }) {
       <ProjectCard project={project} idx={idx} />
     ))
 
-    const featured = projects.filter((project) => (project.feature ? true : false))[0]
-    if (music && atAbout) {
-      projectCards.unshift(<ProjectCard project={featured} idx={11} />)
-    } else if (music && musicChoice === 'electronics') {
-      projectCards.unshift(<ProjectCard project={featured} idx={101} />)
-    } else if (music && featured.category.split(', ').indexOf(musicChoice) >= 0) {
-      projectCards.push(<ProjectCard project={featured} idx={projectCards.length % 2 !== 0 ? 101 : 102} />)
-    } else if (!music && featured.tech) {
-      projectCards.push(<ProjectCard project={featured} idx={projectCards.length % 2 !== 0 ? 101 : 102} />)
-    }
+    // const featured = projects.filter((project) => (project.feature ? true : false))[0]
+    // if (music && atAbout) {
+    //   projectCards.unshift(<ProjectCard project={featured} idx={11} />)
+    // } else if (music && musicChoice === 'electronics') {
+    //   projectCards.unshift(<ProjectCard project={featured} idx={101} />)
+    // } else if (music && featured.category.split(', ').indexOf(musicChoice) >= 0) {
+    //   projectCards.push(<ProjectCard project={featured} idx={projectCards.length % 2 !== 0 ? 101 : 102} />)
+    // } else if (!music) {
+    //   projectCards.push(<ProjectCard project={featured} idx={projectCards.length % 2 !== 0 ? 101 : 102} />)
+    // }
 
     return projects ?
       <section className="projects-body">
         <div className="card-color"></div>
         <h3 className="link flippable" id="project-comp-title">{atAbout ? `RECENT ${music ? "MUSIC" : "CODE"} PROJECTS` : `${music ? "MUSIC" : "CODE"} PROJECTS`}</h3>
-        {music && !atAbout && <div className="music-choices">
-          <button className="music-choice-btn">
-            <h2 className={`music-choice link ${musicChoice === 'percussion' ? " here selected-choice" : ""}`} onClick={(e) => { setMusicChoice('percussion'); }}>
-              PERCUSSION
-            </h2>
-          </button>
+        {music ?
+          !atAbout && <div className="music-choices">
+            <button className="music-choice-btn">
+              <h2 className={`music-choice link ${musicChoice === 'percussion' ? " here selected-choice" : ""}`} onClick={(e) => { setMusicChoice('percussion'); }}>
+                PERCUSSION
+              </h2>
+            </button>
 
-          <button className="music-choice-btn">
-            <h2 className={`music-choice link ${musicChoice === 'electronics' ? " here selected-choice" : ""}`} onClick={(e) => { setMusicChoice('electronics'); }}>
-              ELECTRONICS
-            </h2>
-          </button>
-          <button className="music-choice-btn">
-            <h2 className={`music-choice link ${musicChoice === 'composition' ? " here selected-choice" : ""}`} onClick={(e) => { setMusicChoice('composition'); }}>
-              COMPOSITION
-            </h2>
-          </button>
-          <button className="music-choice-btn">
-            <h2 className={`music-choice link ${musicChoice === 'arrangements' ? " here selected-choice" : ""}`} onClick={(e) => { setMusicChoice('arrangements'); }}>
-              ARRANGEMENTS
-            </h2>
-          </button>
-        </div>}
+            <button className="music-choice-btn">
+              <h2 className={`music-choice link ${musicChoice === 'electronics' ? " here selected-choice" : ""}`} onClick={(e) => { setMusicChoice('electronics'); }}>
+                ELECTRONICS
+              </h2>
+            </button>
+            <button className="music-choice-btn">
+              <h2 className={`music-choice link ${musicChoice === 'composition' ? " here selected-choice" : ""}`} onClick={(e) => { setMusicChoice('composition'); }}>
+                COMPOSITION
+              </h2>
+            </button>
+            <button className="music-choice-btn">
+              <h2 className={`music-choice link ${musicChoice === 'arrangements' ? " here selected-choice" : ""}`} onClick={(e) => { setMusicChoice('arrangements'); }}>
+                ARRANGEMENTS
+              </h2>
+            </button>
+          </div>
+          :
+          !atAbout && <div className="music-choices">
+            <button className="music-choice-btn">
+              <h2 className={`music-choice link ${musicChoice === 'projects' ? " here selected-choice" : ""}`} onClick={(e) => { setMusicChoice('projects'); }}>
+                PROJECTS
+              </h2>
+            </button>
+
+            <button className="music-choice-btn">
+              <h2 className={`music-choice link ${musicChoice === 'videos' ? " here selected-choice" : ""}`} onClick={(e) => { setMusicChoice('videos'); }}>
+                VIDEOS
+              </h2>
+            </button>
+          </div>
+        }
         {projectCards}
       </section>
       :
