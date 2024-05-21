3a07fb8e83217f9bbde5a82339e90867ef7fb680
LouisPino <ljpino95@gmail.com>
2024-05-21 09:33:17 -0400

    molly piece

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,10 @@ export default function ProjectCard({ project, idx }) {
     return <div className="project-card" key={project.name} id={idx}>
         {idx % 2 === 0 ?
             !project.embed && <img className="project-image" src={project.image} /> :
-            project.embed && <iframe width="560" height="315" className={`project-iframe ${project.embedLink.includes('bandcamp') ? "bandcamp-iframe" : ""}`} src={`${project.embedLink}`} frameBorder="0"></iframe>
+            project.embed &&
+            <div className="project-card-iframe-ctr" key={project.name} id={idx}>
+                <iframe width="560" height="315" className={`project-iframe ${project.embedLink.includes('bandcamp') ? "bandcamp-iframe" : ""}`} src={`${project.embedLink}`} frameBorder="0"></iframe>
+            </div>
         }
         <div className="project-info">
             <h1 className="here project-title"> {project.name}</h1>
