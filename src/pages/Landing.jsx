4929e347cb1991c653480bccfeb60e950de7342a
LouisPino <ljpino95@gmail.com>
2025-07-08 17:07:27 -0400

    no landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,7 +4,7 @@ import Welcome from "../components/Welcome"
 export default function LandingPage({ setMusic }) {
     return (
         <>
-            <Welcome message="LOUIS PINO" message2="Code Composer || Sound Scripter" time={1000} />
+            {/* <Welcome message="LOUIS PINO" message2="Code Composer || Sound Scripter" time={1000} /> */}
             <Landing setMusic={setMusic} />
         </>
 
